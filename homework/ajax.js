50cdc09f339f9ba01b344351f0af83e63dcd259c
guhaifeng <1032214318@qq.com>
2018-05-08 17:18:00 +0700

    homework

--- homework/ajax.js
+++ homework/ajax.js
@@ -1,41 +1,45 @@
-function extend(obj1, obj2) {
-    for (var k in obj2) {
-        obj1[k] = obj2[k];
+function Data(data) {
+    var arr = [];
+    for (var i in data) {
+        arr.push(i + '=' + data[i]);
     }
-    return obj1;
+    return arr.join('&');
 }
 
-function ajax(obj) {
-    var Default = {
-        url: '',
-        type: 'get',
-        async: true,
-        data: null,
-        success: null,
-        error: null
-    };
-    var settings = extend(Default, obj);
-    var xhr = null;
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else {
-        xhr = new ActiveXObject('Microsoft.XMLHTTP');
+function ajax(opt) {
+    var json = opt || {};
+    var url = json.url;
+    if (!url) {
+        return;
     }
-    var data = settings.type === 'get' && settings.data ? '?' + settings.data : '';
-    xhr.open(settings.type, settings.url + data, settings.async);
-
-    xhr.onreadystatechange = function() {
-
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                settings.success && settings.success(xhr.responseText);
+    var type = opt.type || 'get';
+    var data = opt.data || {};
+    var async = json.async || true;
+    var xml = null;
+    if (window.XMLHttpRequest) {
+        xml = new XMLHttpRequest();
+    } else {
+        xml = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    xml.onreadystatechange = function() {
+        if (xml.readyState === 4) {
+            if (xml.status === 200) {
+                json.success && json.success(JSON.parse(xml.responseText));
             } else {
-                settings.error && settings.error('获取失败');
+                json.error && json.error(new Error('error!'));
             }
         }
-    }
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    var data1 = settings.type === 'post' && settings.data ? settings.data : null;
+    };
 
-    xhr.send(null);
+    switch (type.toUpperCase()) {
+        case 'GET':
+            xml.open(type, url, async);
+            xml.send(null);
+            break;
+        case 'POST':
+            xml.open(type, url, async);
+            xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xml.send(Data(data));
+            break;
+    }
 }
